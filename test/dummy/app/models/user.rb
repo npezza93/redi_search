6de3326d318ad25aaadf61391b5b54c2313897c3
Nick Pezza <npezza93@gmail.com>
2019-04-19 22:59:42 -0400

    Test add_index, drop_index, and that only AR objects can use this thing

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include RailsRedisSearch
+
+  searchable schema: { name: "TEXT" }
 end
--- test/rails_redis_search_test.rb
+++ test/rails_redis_search_test.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "rails_redis_search"
 
-class RailsRedisSearchTest < TestCase
-  test "that_it_has_a_version_number" do
-    refute_nil ::RailsRedisSearch::VERSION
+class RailsRedisSearchTest < ActiveSupport::TestCase
+  test "index name" do
+    assert_equal "user_idx", User.index_name
+  end
+
+  test "reindex" do
+    assert User.create_index
+    assert User.drop_index
+  end
+
+  test "only ActiveRecord models are searchable" do
+    assert_raises RailsRedisSearch::Error do
+      class Thing
+        include RailsRedisSearch
+      end
+    end
   end
 end
