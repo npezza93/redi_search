9fceebb72e88f12ddefbc5997b623f23867654d0
Nick Pezza <npezza93@gmail.com>
2019-04-19 22:50:03 -0400

    Add railtie and error classes

--- lib/rails_redis_search.rb
+++ lib/rails_redis_search.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+require "rails_redis_search/railtie"
+require "rails_redis_search/error"
 require "rails_redis_search/version"
 
 module RailsRedisSearch
-  class Error < StandardError; end
 end
---
+++ lib/rails_redis_search/railtie.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module RailsRedisSearch
+  class Railtie < ::Rails::Railtie
+  end
+end
