efd00e5114fd5fde2a704dadb2beeeab7e071246
Nick Pezza <npezza93@gmail.com>
2019-04-20 21:41:43 -0400

    utilize the schema

--- lib/rails_redis_search.rb
+++ lib/rails_redis_search.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "active_support/core_ext"
 require "active_support/concern"
 require "active_record/base"
 require "redis"
@@ -8,6 +7,7 @@ require "redis"
 require "rails_redis_search/railtie"
 require "rails_redis_search/error"
 require "rails_redis_search/version"
+require "rails_redis_search/schema"
 
 module RailsRedisSearch
   extend ActiveSupport::Concern
@@ -22,17 +22,21 @@ module RailsRedisSearch
   class_methods do
     attr_reader :index_name, :redis
 
-    def searchable(schema:, index_name: name.underscore + "_idx")
-      @schema = schema
-      @index_name = index_name
+    def searchable(**options)
+      cattr_accessor(
+        :index_name,
+        default: (options[:index_name] || name.underscore + "_idx").to_s
+      )
+      cattr_accessor :schema, default: Schema.new(options[:schema])
+
       @redis = Redis.new(host: "127.0.0.1", port: "6379")
 
       define_singleton_method :create_index do
-        redis.call("FT.CREATE", @index_name, "SCHEMA", @schema.to_a.flatten)
+        redis.call("FT.CREATE", index_name, "SCHEMA", schema.to_s)
       end
 
       define_singleton_method :drop_index do
-        redis.call("FT.DROP", @index_name)
+        redis.call("FT.DROP", index_name)
       end
     end
   end
