ab55b6c491dc13033f64477185c18b5dff9277ef
Nick Pezza <npezza93@gmail.com>
2019-04-23 22:05:06 -0400

    Fix client test

--- test/redi_search/client_test.rb
+++ test/redi_search/client_test.rb
@@ -14,8 +14,8 @@ module RediSearch
     end
 
     test "Response#ok? with other object" do
-      assert Client::Response.new(["field"]).ok?
-      assert_equal ["field"], Client::Response.new(["field"]).ok?
+      assert Client::Response.new({ thing: 1 }).ok?
+      assert_equal({ thing: 1 }, Client::Response.new({ thing: 1 }).ok?)
     end
   end
 end
