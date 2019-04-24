19db7cac7c64e6fc94ef607de2f918b8683012fd
Nick Pezza <npezza93@gmail.com>
2019-04-23 23:24:55 -0400

    Add convenient .search method on models

--- lib/redi_search/model.rb
+++ lib/redi_search/model.rb
@@ -5,15 +5,19 @@ require "redi_search/index"
 module RediSearch
   module Model
     def redi_search(**options)
-      index = Index.new(
-        (options[:index_name] || name.underscore + "_idx").to_s,
-        options[:schema]
-      )
+      index = Index.new(options[:index_name] || "#{name.underscore}_idx",
+                        options[:schema])
 
       class_eval do
         cattr_reader :redi_search_index
 
         class_variable_set :@@redi_search_index, index
+      end
+
+      class << self
+        def search(query)
+          class_variable_get(:@@redi_search_index).search(query)
+        end
       end
     end
   end
