ee0fe44e5979b6ecfd3534455b6fee2b85714f10
Nick Pezza <npezza93@gmail.com>
2019-04-23 21:33:20 -0400

    Drop active support dependency

--- lib/redi_search/schema.rb
+++ lib/redi_search/schema.rb
@@ -4,7 +4,6 @@ require "redi_search/schema/geo_field"
 require "redi_search/schema/numeric_field"
 require "redi_search/schema/tag_field"
 require "redi_search/schema/text_field"
-require "active_support/inflector"
 
 module RediSearch
   class Schema
@@ -17,7 +16,7 @@ module RediSearch
         options = [options] if options.is_a? Symbol
         schema, options = options.to_a.flatten
 
-        "RediSearch::Schema::#{schema.to_s.classify}Field".
+        "RediSearch::Schema::#{schema.to_s.capitalize}Field".
           constantize.
           new(field_name, **options.to_h).
           to_a
