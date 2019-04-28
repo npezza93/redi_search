247d41d177d79a475c6e37850e0733667a483bb1
Nick Pezza <npezza93@gmail.com>
2019-04-28 14:29:19 -0400

    Mkae Collection enumerable

--- lib/redi_search/result/collection.rb
+++ lib/redi_search/result/collection.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 require "redi_search/result"
+require "active_support/core_ext/module/delegation"
 
 module RediSearch
   class Result
     class Collection
+      include Enumerable
+
       attr_reader :count, :records
+      delegate :each, to: :records
 
       def initialize(count, records)
         @count = count
