2f3899f34693000a541971d2fd630e49b429eb73
Nick Pezza <npezza93@gmail.com>
2019-04-30 21:49:09 -0400

    Add query clauses module

---
+++ lib/redi_search/search/clauses.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module RediSearch
+  class Search
+    module Clauses
+      def highlight(**options)
+        clauses.push(*HighlightClause.new(**options).clause)
+
+        self
+      end
+
+      def slop(slop)
+        clauses.push("SLOP", slop)
+
+        self
+      end
+
+      def inorder
+        clauses.push("INORDER")
+
+        self
+      end
+
+      def language(language)
+        clauses.push("LANGUAGE", language)
+
+        self
+      end
+
+      def expander(expander)
+        clauses.push("EXPANDER", expander)
+
+        self
+      end
+
+      def scorer(scorer)
+        clauses.push("SCORER", scorer)
+
+        self
+      end
+
+      def payload(payload)
+        clauses.push("PAYLOAD", payload)
+
+        self
+      end
+
+      def sortby(field, order: :asc)
+        raise ArgumentError unless %i(asc desc).include?(order.to_sym)
+
+        clauses.push("SORTBY", field, order)
+
+        self
+      end
+
+      def limit(offset, num)
+        clauses.push("LIMIT", offset, num)
+
+        self
+      end
+    end
+  end
+end
