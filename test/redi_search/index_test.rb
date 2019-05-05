8cf297f0edf7104db4bffe94a7304e1d840df40e
Nick Pezza <npezza93@gmail.com>
2019-05-05 16:47:39 -0400

    Add some index tests

--- test/redi_search/index_test.rb
+++ test/redi_search/index_test.rb
@@ -23,6 +23,28 @@ module RediSearch
       assert_equal 1, @index.info["num_docs"].to_i
     end
 
+    test "create fails if the index already exists" do
+      dup_index = Index.new("user_idx", first: :text, last: :text)
+
+      refute dup_index.create
+    end
+
+    test "info returns nothing if the index doesnt exist" do
+      rando_idx = Index.new("rando_idx", first: :text, last: :text)
+
+      assert_nil rando_idx.info
+    end
+
+    test "#fields" do
+      assert_equal %w(first last), @index.fields
+    end
+
+    test "#reindex" do
+      assert_equal 0, @index.info["num_docs"].to_i
+      assert @index.reindex(User.all)
+      assert_equal User.count, @index.info["num_docs"].to_i
+    end
+
     test "#search" do
       record = User.create(
         first: Faker::Name.first_name, last: Faker::Name.last_name
