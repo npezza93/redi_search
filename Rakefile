d545c22eef9bb64b61ede7170e406e5d073bc1bf
Nick Pezza <npezza93@gmail.com>
2019-05-05 18:23:50 -0400

    Move simplecov into test_helper

--- Rakefile
+++ Rakefile
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "simplecov"
-
 require "bundler/gem_tasks"
 require "rake/testtask"
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "simplecov"
+
+SimpleCov.start do
+  add_filter "/test/"
+end
+
 require "minitest/pride"
 require "pry"
 require "faker"
