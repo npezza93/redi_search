acfad4a7a54945aaf53ec5f120b92d6ac0110dde
Nick Pezza <npezza93@gmail.com>
2019-05-08 22:04:44 -0400

    Set user fields as phonetic

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  redi_search schema: { first: :text, last: :text }
+  redi_search schema: {
+    first: { text: { phonetic: "dm:en" } },
+    last: { text: { phonetic: "dm:en" } }
+  }
 
   def score
     1.0
