08f7185e2e98dfaa5b2de73895027993e02180d5
Nick Pezza <npezza93@gmail.com>
2019-06-23 15:42:21 -0400

    Refactor clauses test

--- test/redi_search/search/clauses_test.rb
+++ test/redi_search/search/clauses_test.rb
@@ -16,111 +16,87 @@ module RediSearch
       end
 
       test "slop clause" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` SLOP 1", query.slop(1).to_redis
-        )
+        assert_clause("SLOP 1", stubbed_search.slop(1))
       end
 
       test "#in_order clause" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` INORDER", query.in_order.to_redis
-        )
+        assert_clause("INORDER", stubbed_search.in_order)
       end
 
       test "#verbatim clause" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` VERBATIM", query.verbatim.to_redis
-        )
+        assert_clause("VERBATIM", stubbed_search.verbatim)
       end
 
       test "#no_stop_words clause" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` NOSTOPWORDS", query.no_stop_words.to_redis
-        )
+        assert_clause("NOSTOPWORDS", stubbed_search.no_stop_words)
       end
 
       test "#language clause" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` LANGUAGE danish",
-          query.language("danish").to_redis
-        )
+        assert_clause("LANGUAGE danish", stubbed_search.language("danish"))
       end
 
       test "#sort_by clause" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` SORTBY first asc",
-          query.sort_by(:first).to_redis
-        )
+        assert_clause("SORTBY first asc", stubbed_search.sort_by(:first))
       end
 
       test "#sort_by desc clause" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` SORTBY first desc",
-          query.sort_by(:first, order: :desc).to_redis
+        assert_clause(
+          "SORTBY first desc", stubbed_search.sort_by(:first, order: :desc)
         )
       end
 
       test "#sort_by arg error with bad order" do
-        query = RediSearch::Search.new(@index, "dr")
-
         assert_raise ActiveModel::ValidationError do
-          query.sort_by(:first, order: :random)
+          stubbed_search.sort_by(:first, order: :random)
         end
       end
 
       test "#limit clause defaults to 0 offset" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` LIMIT 0 10",
-          query.limit(10).to_redis
-        )
+        assert_clause("LIMIT 0 10", stubbed_search.limit(10))
       end
 
       test "#limit clause with custom offset" do
-        query = RediSearch::Search.new(@index, "dr")
-
-        assert_equal(
-          "SEARCH users_test `dr` LIMIT 5 10",
-          query.limit(10, 5).to_redis
-        )
+        assert_clause("LIMIT 5 10", stubbed_search.limit(10, 5))
       end
 
       test "no_content just returns docs with ids" do
-        Name = Struct.new(:name) do
-          def id
-            SecureRandom.hex
-          end
-        end
-        User.find_each do |user|
-          @index.add Document.for_object(@index, Name.new(user.first))
-        end
+        index_all_users
 
         assert_nothing_raised do
           assert_not @index.search("*").no_content.results.empty?
         end
       end
 
-      test "return on returns certain fields" do
-        query = RediSearch::Search.new(@index, "dr")
+      test "#return returns certain fields" do
+        assert_clause("RETURN 1 name", stubbed_search.return(:name))
+      end
 
         assert_equal(
-          "SEARCH users_test `dr` RETURN 1 name",
-          query.return(:name).to_redis
+        )
+      end
+
+
+      private
+
+      Name = Struct.new(:name) do
+        def id
+          SecureRandom.hex
+        end
+      end
+
+      def index_all_users
+        User.find_each do |user|
+          @index.add Document.for_object(@index, Name.new(user.first))
+        end
+      end
+
+      def stubbed_search
+        RediSearch::Search.new(@index, "dr")
+      end
+
+      def assert_clause(expected_clause, query)
+        assert_equal(
+          "SEARCH users_test `dr` #{expected_clause}", query.to_redis
         )
       end
     end
