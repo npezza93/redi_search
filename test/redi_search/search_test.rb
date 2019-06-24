14851e0bcba3679cae878f3c9d6b34857869d1ab
Nick Pezza <npezza93@gmail.com>
2019-06-23 22:23:10 -0400

    Add more query tests

--- test/redi_search/search_test.rb
+++ test/redi_search/search_test.rb
@@ -129,12 +129,45 @@ module RediSearch
       )
     end
 
-    # test "union inside phrase" do
-    #   query = User.search("obama").or "barack", "barrack"
-    #
-    #   assert_equal(
-    #     "SEARCH users_test \"`hello` -(`world`|`werld`)\"", query.to_redis
-    #   )
-    # end
+    test "union inside phrase" do
+      query = User.search("obama").and(User.search("barack").or("barrack"))
+
+      assert_equal(
+        "SEARCH users_test \"`obama` (`barack`|`barrack`)\"", query.to_redis
+      )
+    end
+
+    test "multiple optional terms with higher priority" do
+      query = User.search("obama").and("barack", optional: true).and(
+        "michelle", optional: true
+      )
+
+      assert_equal(
+        "SEARCH users_test \"`obama` `~barack` `~michelle`\"", query.to_redis
+      )
+    end
+
+    test "exact phrase in one field, one word in another field" do
+      query =
+        User.search.where(title: "barack obama").where(job: "president")
+
+      assert_equal(
+        "SEARCH users_test (@title:`barack obama`) (@job:`president`)",
+        query.to_redis
+      )
+    end
+
+    test "combined AND, OR with field specifiers" do
+      query = User.search("world").where(title: "hello").where(
+        body: User.search("foo").and("bar")
+      ).where(category: User.search("articles").or("biographies"))
+
+      expected = <<~CLAUSE.squish
+        `world` (@title:`hello`) (@body:(`foo` `bar`))
+        (@category:(`articles`|`biographies`))
+      CLAUSE
+
+      assert_equal("SEARCH users_test \"#{expected}\"", query.to_redis)
+    end
   end
 end
