38244c812e2a0173e76c475ddc7e6ffe5f20959c
Nick Pezza <npezza93@gmail.com>
2019-06-27 21:56:48 -0400

    Move making a schema field instance to a class method

--- lib/redi_search/schema.rb
+++ lib/redi_search/schema.rb
@@ -7,19 +7,23 @@ require "redi_search/schema/text_field"
 
 module RediSearch
   class Schema
+    def self.make_field(field_name, options)
+      options = [options] if options.is_a? Symbol
+      schema, options = options.to_a.flatten
+
+      "RediSearch::Schema::#{schema.to_s.capitalize}Field".
+        constantize.
+        new(field_name, **options.to_h).
+        to_a
+    end
+
     def initialize(raw)
       @raw = raw
     end
 
     def to_a
       raw.map do |field_name, options|
-        options = [options] if options.is_a? Symbol
-        schema, options = options.to_a.flatten
-
-        "RediSearch::Schema::#{schema.to_s.capitalize}Field".
-          constantize.
-          new(field_name, **options.to_h).
-          to_a
+        self.class.make_field(field_name, options)
       end.flatten
     end
 
