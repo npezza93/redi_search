59ce0494da97820279f770cb37bc2f4a835510f4
Nick Pezza <npezza93@gmail.com>
2019-07-17 08:18:06 -0400

    Fixup document test

--- test/redi_search/document_test.rb
+++ test/redi_search/document_test.rb
@@ -5,8 +5,10 @@ require "redi_search/document"
 
 module RediSearch
   class DocumentTest < Minitest::Test
+    include ActiveSupport::Testing::Assertions
+
     def setup
-      @index = Index.new("users_test", first: :text, last: :text)
+      @index = Index.new(:users_test, first: :text, last: :text)
       @index.drop
       @index.create
     end
@@ -26,10 +28,9 @@ module RediSearch
     end
 
     def test_get_class_method
-      assert_difference -> { User.redi_search_index.document_count } do
-        @record = User.create(
-          first: Faker::Name.first_name, last: Faker::Name.last_name
-        )
+      assert_difference -> { @index.document_count } do
+        @record = users(index: 0)
+        @index.add(Document.for_object(@index, @record))
       end
 
       doc = RediSearch::Document.get(@index, @record.id)
@@ -45,42 +46,38 @@ module RediSearch
     end
 
     def test_mget_class_method
-      assert_difference -> { User.redi_search_index.document_count }, 2 do
-        @record1 = User.create(
-          first: Faker::Name.first_name, last: Faker::Name.last_name
-        )
-        @record2 = User.create(
-          first: Faker::Name.first_name, last: Faker::Name.last_name
-        )
+      assert_difference -> { @index.document_count }, 2 do
+        @record1 = users(index: 0)
+        @record2 = users(index: 1)
+        @index.add(Document.for_object(@index, @record1))
+        @index.add(Document.for_object(@index, @record2))
       end
 
       docs = RediSearch::Document.mget(@index, @record1.id, @record2.id)
       assert_equal 2, docs.count
-      assert_equal @record1.id, docs.first.document_id_without_index
-      assert_equal @record2.id, docs.second.document_id_without_index
+      assert_equal @record1.id, docs[0].document_id_without_index
+      assert_equal @record2.id, docs[1].document_id_without_index
     end
 
     def test_mget_class_method_when_a_doc_doesnt_exist
-      assert_difference -> { User.redi_search_index.document_count } do
-        @record1 = User.create(
-          first: Faker::Name.first_name, last: Faker::Name.last_name
-        )
+      assert_difference -> { @index.document_count } do
+        @record = users(index: 0)
+        @index.add(Document.for_object(@index, @record))
       end
 
-      docs = RediSearch::Document.mget(@index, @record1.id, "rando")
+      docs = RediSearch::Document.mget(@index, @record.id, "rando")
       assert_equal 1, docs.count
-      assert_equal @record1.id, docs.first.document_id_without_index
-      assert_nil docs.second
+      assert_equal @record.id, docs[0].document_id_without_index
+      assert_nil docs[1]
     end
 
     def test_del
-      assert_difference -> { User.redi_search_index.document_count } do
-        @record1 = User.create(
-          first: Faker::Name.first_name, last: Faker::Name.last_name
-        )
+      assert_difference -> { @index.document_count } do
+        @record = users(index: 0)
+        @index.add(Document.for_object(@index, @record))
       end
 
-      doc = RediSearch::Document.get(@index, @record1.id)
+      doc = RediSearch::Document.get(@index, @record.id)
       assert doc.del
       assert_equal 0, @index.info["num_docs"].to_i
     end
