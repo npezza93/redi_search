d9c36cc550dc20532d6151f2437cdd9a0a46d71f
Nick Pezza <npezza93@gmail.com>
2019-08-01 19:58:02 -0400

    Reset env variables so default is dev

--- test/unit/redi_search_test.rb
+++ test/unit/redi_search_test.rb
@@ -5,6 +5,8 @@ require "redi_search"
 
 class RediSearchTest < Minitest::Test
   def test_env_is_dev_by_default
+    ENV["RAILS_ENV"] = ENV["RACK_ENV"] = nil
     assert_equal "development", RediSearch.env
+    ENV["RAILS_ENV"] = ENV["RACK_ENV"] = "test"
   end
 end
