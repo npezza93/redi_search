4e8f3cc346ccbf602abde1a76a6a4aae47e075f6
Nick Pezza <npezza93@gmail.com>
2019-08-01 17:38:24 -0400

    Add result test

---
+++ test/unit/redi_search/search/result_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module RediSearch
+  class Search
+    class ResultTest < Minitest::Test
+      def setup
+        search = Search.new(Index.new(:users, name: :text), "foo")
+        @result = Result.new(search, 1, ["users1", %w(name foo)])
+      end
+
+      def test_count
+        assert_equal 1, @result.count
+      end
+
+      def test_size
+        assert_equal 1, @result.size
+      end
+
+      def test_document_parsing
+        assert_equal "users1", @result[0].document_id
+        assert_equal "foo", @result[0].name
+      end
+    end
+  end
+end
