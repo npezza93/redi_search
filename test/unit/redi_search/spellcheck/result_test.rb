4118bed831239eda22986232a958ac2b52c86190
Nick Pezza <pezza@hey.com>
2021-09-15 18:46:36 -0400

    Fix cop

--- test/unit/redi_search/spellcheck/result_test.rb
+++ test/unit/redi_search/spellcheck/result_test.rb
@@ -9,8 +9,8 @@ module RediSearch
       def test_inspect
         inspection = Result.new("foo", [["0.5", "foob"]]).inspect
         expected_inspection = "#<RediSearch::Spellcheck::Result term: foo, "\
-                              "suggestions: [#<struct RediSearch::Spellcheck::Suggestion score=0.5,"\
-                              " suggestion=\"foob\">]>"
+                              "suggestions: [#<struct RediSearch::Spellcheck::"\
+                              "Suggestion score=0.5, suggestion=\"foob\">]>"
 
         assert_equal expected_inspection, inspection
       end
